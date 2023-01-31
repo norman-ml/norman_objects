9f249038a39b0e16cd3a5f2eeed3384c0fa41fb3
Yonatan <yonatan@backbox.com>
2023-01-31 20:48:17 +0200

    Added UrlRequestType enum to model schema.

--- norman_objects/models/model.py
+++ norman_objects/models/model.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 from norman_objects.models.model_hosting_location import ModelHostingLocation
 from norman_objects.models.model_type import ModelType
+from norman_objects.models.url_request_type import UrlRequestType
 from norman_objects.parameters.model_param import ModelParam
 
 
@@ -11,7 +12,7 @@ class Model(BaseModel):
     name: str
     url: str
     logo_url: str
-    url_request_type: str
+    url_request_type: UrlRequestType
     model_type: ModelType
     model_hosting_location: ModelHostingLocation
     short_description: str
---
+++ norman_objects/models/url_request_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class UrlRequestType(Enum):
+    Get = "Get"
+    Post = "Post"
+    Put = "Put"
