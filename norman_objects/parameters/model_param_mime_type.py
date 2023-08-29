dece1fbf32791853ea3314c32249f8e4f7ad29d2
Yonatan <yonatan@backbox.com>
2023-08-30 01:53:07 +0300

    Added some video mime types to model_param_mime_type

--- norman_objects/parameters/model_param_mime_type.py
+++ norman_objects/parameters/model_param_mime_type.py
@@ -12,6 +12,9 @@ class ModelParamMimeType(str, Enum):
     Image_Png = "Image_Png"
     Image_Webp = "Image_Webp"
     Text_Plain = "Text_Plain"
+    Video_Mp4 = "Video_Mp4"
+    Video_Ogg = "Video_Ogg"
+    Video_Webm = "Video_Webm"
 
     @property
     def mime_type(self):
