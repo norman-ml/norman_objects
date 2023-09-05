1cc1c46e2c66de0d9f4b339dab7095230aabec21
Yonatan <yonatan@backbox.com>
2023-09-05 03:23:26 +0300

    Corrected signature transform schema to reference signatures, instead of params

--- norman_objects/signatures/signature_transform.py
+++ norman_objects/signatures/signature_transform.py
@@ -5,6 +5,6 @@ from pydantic import BaseModel
 
 class SignatureTransform(BaseModel):
     id: str = "0"
-    param_id: str = "0"
+    signature_id: str = "0"
     transform_name: str
     transform_args: Dict[str, str] = {}
