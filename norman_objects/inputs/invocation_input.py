3a93b0826e75d7056f07727c7f0924b1ac8e8d3f
Yonatan <yonatan@backbox.com>
2023-09-05 04:49:46 +0300

    Corrected invocation inputs and outputs to reference signatures, instead of params

--- norman_objects/inputs/invocation_input.py
+++ norman_objects/inputs/invocation_input.py
@@ -6,7 +6,7 @@ from norman_objects.inputs.input_source import InputSource
 class InvocationInput(BaseModel):
     id: str = "0"
     model_id: str
-    parameter_id: str
+    signature_id: str
     invocation_id: str = "0"
     input_source: InputSource
     url: str = ""
--- norman_objects/outputs/invocation_output.py
+++ norman_objects/outputs/invocation_output.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel
 class InvocationOutput(BaseModel):
     id: str = "0"
     model_id: str
-    parameter_id: str
+    signature_id: str
     invocation_id: str = "0"
