de763de441abb40f2dd56c7d22fced41dea34377
Dolev <dolev.gabay@norman-ai.com>
2024-12-15 12:47:56 +0200

    history invocations

---
+++ norman_objects/invocations/invocation_history.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timezone, timedelta
+from pydantic import BaseModel, Field
+
+class InvocationHistory(BaseModel):
+    id: str
+    model_id: str
+    creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
+    flag_name: str
+    model_name: str
+    asset_id: str
+    asset_name: str
