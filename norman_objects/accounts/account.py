3ed951a44f2d38927be7ccff0620fb9933696bff
tomer.israeli <tomer.israeli@norman-ai.com>
2024-12-18 16:46:29 +0200

    the EmailStr made problem with the other services work. deleted it and I will try to add it later.
    [#86c1996qt]

--- norman_objects/accounts/account.py
+++ norman_objects/accounts/account.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta, timezone
 from typing import Optional
-from pydantic import EmailStr
 from pydantic import BaseModel, Field
 
 class Account(BaseModel):
     id: str = "0"
     creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     name: str
-    email: Optional[EmailStr] = None
+    email: Optional[str] = None
