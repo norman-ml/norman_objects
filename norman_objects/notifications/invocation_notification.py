3fac9ac5a8dd66788ee133df0c5ea9f7cf363422
Dolev <dolev.gabay@norman-ai.com>
2024-12-22 19:15:53 +0200

    model name

--- norman_objects/notifications/invocation_notification.py
+++ norman_objects/notifications/invocation_notification.py
@@ -7,7 +7,7 @@ from norman_objects.notifications.severity import Severity
 class InvocationNotification(BaseModel):
     id: Optional[str] = None
     entity_id: Optional[str] = None
-    entity_name: Optional[str] = None
+    model_name: Optional[str] = None
     creation_time: Optional[datetime] = None
     title: str
     message: str
