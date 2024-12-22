89e6e21187086d78e41801ab6058f02af07ab8d6
Dolev <dolev.gabay@norman-ai.com>
2024-12-22 15:12:56 +0200

    add entity id

--- norman_objects/notifications/notification.py
+++ norman_objects/notifications/notification.py
@@ -6,6 +6,7 @@ from norman_objects.notifications.severity import Severity
 
 class Notification(BaseModel):
     id: Optional[str] = None
+    entity_id: Optional[str] = None
     creation_time: Optional[datetime] = None
     title: str
     message: str
