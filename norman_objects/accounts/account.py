95f9b0cde1134bed7947eb68b7473653929860cc
tomer.israeli <tomer.israeli@norman-ai.com>
2024-12-24 19:05:29 +0200

    changed the pass_hash field name to password.

--- norman_objects/accounts/account.py
+++ norman_objects/accounts/account.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-
 class Account(BaseModel):
     id: str = "0"
     creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     name: str
     email: Optional[str] = None
+    password: str
