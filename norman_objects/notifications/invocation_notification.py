34de534d0dced69068a601f9eb8363e41db70314
Dolev <dolev.gabay@norman-ai.com>
2024-12-31 12:24:08 +0200

    cr fixes

--- norman_objects/notifications/invocation_notification.py
+++ norman_objects/notifications/invocation_notification.py
@@ -1,15 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 from pydantic import BaseModel, Field
 from norman_objects.notifications.severity import Severity
 
 class InvocationNotification(BaseModel):
-    id: Optional[str] = None
-    entity_id: Optional[str] = None
+    id: str = None
+    account_id: str = None
+    entity_id: str = None
+    creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     model_name: Optional[str] = None
-    creation_time: Optional[datetime] = None
     title: str
     message: str
-    read_status: int = 0
+    read_status: int
     severity: Severity
--- norman_objects/notifications/severity.py
+++ norman_objects/notifications/severity.py
@@ -4,6 +4,6 @@ from enum import Enum
 class Severity(str, Enum):
     ERROR = "error"
     INFO = "info"
-    WARNING = "warning"
     SUCCESS = "success"
+    WARNING = "warning"
 
