5f1b14175f881c9fb248950e59e41de225e8743f
tomer.israeli <tomer.israeli@norman-ai.com>
2025-01-02 10:02:40 +0200

    email field change from optional Null to default value of ""
    (#86c1996qt)

--- norman_objects/accounts/account.py
+++ norman_objects/accounts/account.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta, timezone
-from typing import Optional
 from pydantic import BaseModel, Field
 
 class Account(BaseModel):
     id: str = "0"
     creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     name: str
-    email: Optional[str] = None
+    email: str = ""
