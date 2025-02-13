d660bbff5ceea1c556568cb66fde11328e2bda08
Dolev <dolev.gabay@norman-ai.com>
2025-02-13 16:54:10 +0200

    add guest id and user id

--- norman_objects/accounts/account.py
+++ norman_objects/accounts/account.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel, Field
 
 class Account(BaseModel):
     id: str = "0"
+    guest_id: Optional[str] = None
+    user_id: Optional[str] = None
     creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     name: str
     email: Optional[str] = None
