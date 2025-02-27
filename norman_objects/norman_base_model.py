a23449ba034d953751ad6ad634d575542f752727
Dolev <dolev.gabay@norman-ai.com>
2025-02-27 15:42:46 +0200

    add .items to field_dictionary

--- norman_objects/norman_base_model.py
+++ norman_objects/norman_base_model.py
@@ -15,7 +15,7 @@ class NormanBaseModel(BaseModel):
 
     def to_sql_fields(self):
         field_dictionary = self.dict(exclude_unset=True)
-        sql_dictionary = {self.to_sql_field_name(key): value for key, value in field_dictionary}
+        sql_dictionary = {self.to_sql_field_name(key): value for key, value in field_dictionary.items()}
         return sql_dictionary
 
     def to_sql_field_name(self, field_name: str):
