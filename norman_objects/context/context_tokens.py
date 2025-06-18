2af751fb834fb77df1e95d3e9816976eeb181026
Dolev <dolev.gabay@norman-ai.com>
2025-06-18 15:31:55 +0300

    cr fixes

--- norman_objects/context/context_tokens.py
+++ norman_objects/context/context_tokens.py
@@ -3,5 +3,5 @@ from contextvars import ContextVar
 from norman_objects.sensitive.sensitive_type import SensitiveType
 
 class NormanContext:
-    raw_access_token: ContextVar[SensitiveType(str)] = ContextVar("norman_raw_access_token", default=None)
+    access_token: ContextVar[SensitiveType(str)] = ContextVar("norman_access_token", default=None)
     decoded_access_token: ContextVar[SensitiveType(dict)] = ContextVar("norman_decoded_access_token", default=None)
