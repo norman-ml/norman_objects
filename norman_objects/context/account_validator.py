f39243371fba01cab21ff4df98c18088cb4cccc4
Dolev <dolev.gabay@norman-ai.com>
2025-07-01 18:51:38 +0300

    Remove norman utils use in norman objects prevent circular dependency.
    #86c45fm85

--- norman_objects/context/account_validator.py
+++ norman_objects/context/account_validator.py
@@ -1,22 +1,15 @@
 from contextlib import ContextDecorator
 
 from norman_objects.context.context_tokens import NormanContext
-from norman_utils.cloud.cognito_utils import CognitoUtils
 
 
 class AccountValidator(ContextDecorator):
     def __init__(self, expected_account_id: str):
-        self.__cognito_utils = CognitoUtils()
         self.expected_account_id = expected_account_id
 
     def __enter__(self):
         decoded_token = NormanContext.decoded_access_token.get(None)
-        if decoded_token is not None:
-            token_account_id = decoded_token.get("cognito:username")
-        else:
-            access_token = NormanContext.access_token.get(None)
-            raw_decoded_token = self.__cognito_utils.decode_jwt_token(access_token)
-            token_account_id = raw_decoded_token.get("cognito:username")
+        token_account_id = decoded_token.get("cognito:username")
 
         if token_account_id != self.expected_account_id:
             raise PermissionError("Account ID mismatch. Access denied.")
