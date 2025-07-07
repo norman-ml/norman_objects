26efdd9ca07a62c4021465cffcd832a51410b7aa
frankos333 <frankos333@gmail.com>
2025-07-07 14:53:22 +0300

    changing the invocation_notification object - it now inheris from notification, and creating a new object model_notification.py which will also inherit from notification.

--- norman_objects/notifications/__init__.py
+++ norman_objects/notifications/__init__.py
@@ -1,2 +1,5 @@
+import norman_objects.notifications.invocation_notification
+import norman_objects.notifications.model_notification
 import norman_objects.notifications.notification
+import norman_objects.notifications.notification_type
 import norman_objects.notifications.severity
