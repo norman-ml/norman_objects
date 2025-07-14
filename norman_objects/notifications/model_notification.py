9106dca59304349e62448217b2ffd83dd4ba47ca
frankos333 <frankos333@gmail.com>
2025-07-14 10:57:35 +0300

    getting back to one notification object for both model and invocation notifications
    (86c1m1z8u)

--- norman_objects/notifications/model_notification.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta, timezone
-from typing import Optional
-
-from norman_objects.notifications.notification import Notification
-
-class ModelNotification(Notification):
-    model_name: Optional[str] = None
