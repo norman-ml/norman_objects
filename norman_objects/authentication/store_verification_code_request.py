7eb7fa31cb1ba3d0d5d44b0b5331227de937f2b0
Dolev <dolev.gabay@norman-ai.com>
2025-07-24 11:14:10 +0300

    Reorder: added Creation time to store_verification_code_request.py.
    (#86c4g2qr6)

--- norman_objects/authentication/store_verification_code_request.py
+++ norman_objects/authentication/store_verification_code_request.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta, timezone
+
 from norman_objects.norman_base_model import NormanBaseModel
+from pydantic import Field
 
 
 class StoreVerificationCodeRequest(NormanBaseModel):
     account_id: str
     email: str
     code: str
-    expiration_seconds: int = 300  # default 5 minutes
+    expiration_time: datetime
+    creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
+
