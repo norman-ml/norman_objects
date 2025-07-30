2e336ca729b5fe58b38dac71c11996163b080f78
Dolev <dolev.gabay@norman-ai.com>
2025-07-30 13:12:40 +0300

    Refactor: reorder the Verefication code properties.
    (#86c4t4zbp)

--- norman_objects/authentication/verification_code.py
+++ norman_objects/authentication/verification_code.py
@@ -9,7 +9,7 @@ class VerificationCode(NormanBaseModel):
     id: str = "0"
     account_id: str
     creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
+    expiration: datetime
     email: str
     code: SensitiveType(str)
-    expiration: datetime
     verified: int = 0
