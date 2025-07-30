d5aa09b55d04ececf8714386101ccb7fc052bf22
frankos333 <frankos333@gmail.com>
2025-07-30 12:07:27 +0300

    Order of functions in NormanContext. its is a very good context.
    (#860rm962c)

--- norman_objects/context/context_tokens.py
+++ norman_objects/context/context_tokens.py
@@ -3,28 +3,28 @@ from norman_objects.sensitive.sensitive import Sensitive
 
 
 class NormanContext:
-    _access_token = ContextVar("norman_access_token", default=None)
-    _decoded_access_token = ContextVar("norman_decoded_access_token", default=None)
-
-    @staticmethod
-    def set_access_token(token: Sensitive):
-        NormanContext._access_token.set(token)
+    _access_token = ContextVar("norman_access_token")
+    _decoded_access_token = ContextVar("norman_decoded_access_token")
 
     @staticmethod
     def get_access_token():
         return NormanContext._access_token.get(None)
 
     @staticmethod
+    def set_access_token(token: Sensitive):
+        NormanContext._access_token.set(token)
+
+    @staticmethod
     def clear_access_token():
         NormanContext._access_token.set(None)
 
     @staticmethod
-    def set_decoded_access_token(decoded: Sensitive):
-        NormanContext._decoded_access_token.set(decoded)
-
-    @staticmethod
     def get_decoded_access_token():
         return NormanContext._decoded_access_token.get(None)
+
+    @staticmethod
+    def set_decoded_access_token(decoded: Sensitive):
+        NormanContext._decoded_access_token.set(decoded)
 
     @staticmethod
     def clear_decoded_access_token():
