9c9aa119142ded3bfb5cfdd6beb8e77d68d851e3
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-08-03 01:49:56 +0300

    Modified InvocationParams class to match behaviour of class backed message instances; thus passing model and invocation into norman lambdas and saving them querying the database.
    (#86c4tna58)

--- norman_objects/invocations/invocation_params.py
+++ norman_objects/invocations/invocation_params.py
@@ -1,11 +1,10 @@
-from typing import List
-
+from norman_objects.invocations.invocation import Invocation
+from norman_objects.models.model import Model
 from norman_objects.norman_base_model import NormanBaseModel
 from norman_objects.sensitive.sensitive_type import SensitiveType
 
 
 class InvocationParams(NormanBaseModel):
     access_token: SensitiveType(str)
-    model_id: str
-    invocation_id: str
-    input_ids: List[str]
+    model: Model
+    invocation: Invocation
