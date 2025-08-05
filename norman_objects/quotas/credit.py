8f94f0d05ba647f2507856a2fc7de4e1dcaddac8
Dolev <dolev.gabay@norman-ai.com>
2025-08-05 15:03:59 +0300

    Create a "now" constractor to credit object in norman objects. (#86c43x96k)

--- norman_objects/quotas/credit.py
+++ norman_objects/quotas/credit.py
@@ -1,9 +1,20 @@
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta, UTC
 
 class Credit(BaseModel):
-    id: str = "0"                   # default means "auto-generate"
-    quota_id: str                   # UUID (BINARY(16) in DB)
+    id: str = "0"
+    quota_id: str
     start_date: datetime
     end_date: datetime
     billable: bool = False
+
+    @classmethod
+    def now(cls, quota_id: str, duration_seconds: int, billable: bool):
+        now = datetime.now(UTC)
+        return cls(
+            id="0",
+            quota_id=quota_id,
+            start_date=now,
+            end_date=now + timedelta(seconds=duration_seconds),
+            billable=billable
+        )
