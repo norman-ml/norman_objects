6335eb1b22aec76d69ed0e46808c1418af423ed5
Dolev <dolev.gabay@norman-ai.com>
2025-08-06 21:01:22 +0300

    Generate guet user account and not guest user properites.
    (#86c4g2qr6)

--- norman_objects/accounts/account.py
+++ norman_objects/accounts/account.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 
-from norman_objects.authentication.signup_request import SignupRequest
-from norman_objects.authentication.user_properties import UserProperties
 from norman_objects.norman_base_model import NormanBaseModel
 from pydantic import Field
 
@@ -14,13 +12,3 @@ class Account(NormanBaseModel):
     email: Optional[str] = None
     registered: int
     confirmed: int
-
-    @staticmethod
-    def from_signup_request(account_id: str, signup_request: SignupRequest, registered: bool, confirmed: bool):
-        return Account(
-            id=account_id,
-            name=signup_request.name,
-            email=signup_request.email,
-            registered=registered,
-            confirmed=confirmed,
-        )
