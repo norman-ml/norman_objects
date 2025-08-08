92e3ab416624990fc2bd55eaa32c27daea159a7f
frankos333 <frankos333@gmail.com>
2025-08-08 18:28:02 +0300

    Make table optional in SortNode to prevent errors when Pydantic builds constraints from client requests.
    (#86c4qxme1)

--- norman_objects/queries/sort_clauses/sort_node.py
+++ norman_objects/queries/sort_clauses/sort_node.py
@@ -1,15 +1,18 @@
-from typing import Dict, Set
+from typing import Dict, Set, Optional
 
 from norman_objects.norman_base_model import NormanBaseModel
 from norman_objects.queries.sort_clauses.sort_direction import SortDirection
 
 
 class SortNode(NormanBaseModel):
-    table: str
+    table: Optional[str] = None
     column: str = "ID"
     direction: SortDirection = SortDirection.ASC
 
     def validate_expression(self, allowed_tables_and_columns: Dict[str, Set[str]]):
+        if self.table is None:
+            return True
+
         if self.table not in allowed_tables_and_columns:
             return False
 
@@ -19,4 +22,7 @@ class SortNode(NormanBaseModel):
         return True
 
     def build_expression(self):
-        return f" {self.table}.{self.column} {self.direction.value} "
+        if self.table is None:
+            return f" {self.column} {self.direction.value} "
+        else:
+            return f" {self.table}.{self.column} {self.direction.value} "
