e6a474155a5cfe745a40bc86ab76af35be2f3e25
Dolev <dolev.gabay@norman-ai.com>
2025-08-11 19:58:24 +0300

    Added email to verification code and confirm_email_request to valid the email is match.
    (#86c4g2qr6)

--- norman_objects/authentication/verification_code.py
+++ norman_objects/authentication/verification_code.py
@@ -10,5 +10,6 @@ class VerificationCode(NormanBaseModel):
     account_id: str
     creation_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     expiration_time: datetime
+    email: str
     confirmed: bool = False
     code: SensitiveType(str)
