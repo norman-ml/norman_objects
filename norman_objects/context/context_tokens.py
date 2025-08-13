db31d875fff53a3dafd1e8adfe7d9042bc1b8cff
frankos333 <frankos333@gmail.com>
2025-08-13 15:53:43 +0300

    Adapting to working with jwt_decode output as a dict of header,payload and hmac.
    (#86c4qxme1)

--- norman_objects/context/context_tokens.py
+++ norman_objects/context/context_tokens.py
@@ -1,29 +1,29 @@
-from contextvars import ContextVar
 from typing import Optional
-
+from contextvars import ContextVar
 from norman_objects.tokens.access_token import AccessToken
-
+from norman_objects.sensitive.sensitive_type import SensitiveType
 
 class NormanContext:
     _access_token: ContextVar[Optional[AccessToken]] = ContextVar("norman_access_token", default=None)
 
-
     @staticmethod
-    def get_access_token_object():
+    def get_access_token_object() -> Optional[AccessToken]:
         return NormanContext._access_token.get()
 
     @staticmethod
     def get_access_token():
-        return NormanContext._access_token.get().get_access_token()
+        tok = NormanContext._access_token.get()
+        return tok.get_access_token() if tok is not None else None  # returns SensitiveType(str) or None
 
     @staticmethod
-    def get_decoded_access_token():
-        return NormanContext._access_token.get().get_decoded_access_token()
+    def get_decoded_access_token() -> Optional[dict]:
+        tok = NormanContext._access_token.get()
+        return tok.get_decoded_access_token() if tok is not None else None
 
     @staticmethod
-    def set_access_token(access_token: AccessToken):
+    def set_access_token(access_token: AccessToken) -> None:
         NormanContext._access_token.set(access_token)
 
     @staticmethod
-    def clear():
+    def clear() -> None:
         NormanContext._access_token.set(None)
