4e6bb4aed25524cb7f1bfd97847babe6968a9e16
avremy <avremy.back@norman-ai.com>
2025-08-14 18:24:32 +0300

    Refactor imports and type annotations to support Python 3.9.
    (#86c50w30u)

--- norman_objects/queries/filter_clauses/filter_value_type.py
+++ norman_objects/queries/filter_clauses/filter_value_type.py
@@ -1,4 +1,8 @@
-from typing import TypeAlias, Union
+try:
+    from typing import TypeAlias, Union
+except ImportError:
+    from typing_extensions import TypeAlias
+    from typing import Union
 
 FilterTypeValue: TypeAlias = Union[str, int, float]
 FilterTypeVar: TypeAlias = Union[FilterTypeValue, list[FilterTypeValue]]
