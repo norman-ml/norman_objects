25c947857f92fea5e0019f8188738470098bb475
frankos333 <frankos333@gmail.com>
2025-08-17 20:53:08 +0300

    Changes from CR wth yonatan on google meet. these are great changes. we like them very much.
    (#86c4qxme1)

--- norman_objects/context/context_tokens.py
+++ norman_objects/context/context_tokens.py
@@ -1,10 +1,11 @@
 from contextvars import ContextVar
+from typing import Optional
 
 from norman_objects.authorization.access_token import AccessToken
 
 
 class NormanContext:
-    _access_token: ContextVar[AccessToken] = ContextVar("norman_access_token")
+    _access_token: ContextVar[Optional[AccessToken]] = ContextVar("norman_access_token")
 
     @staticmethod
     def get_access_token():
