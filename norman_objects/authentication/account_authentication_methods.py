94c9e9d1a94f3c9526d79d9f8c5384fb5bbb1f52
Dolev <dolev.gabay@norman-ai.com>
2025-08-18 19:35:23 +0300

    Refactor(view): rename columns in Account_Authentication_Methods for clarity.
    (#86c4g2qr6)

--- norman_objects/authentication/account_authentication_methods.py
+++ norman_objects/authentication/account_authentication_methods.py
@@ -3,9 +3,9 @@ from norman_objects.norman_base_model import NormanBaseModel
 
 class AccountAuthenticationMethods(NormanBaseModel):
     account_id: str
-    api_keys_count: int
-    passwords_count: int
-    verified_emails_count: int
+    api_key_count: int
+    password_count: int
+    verified_email_count: int
 
     def has_method_configured(self):
         return self.api_keys_count > 0 or self.passwords_count > 0 or self.verified_emails_count > 0
