2e89bdebb04c62aeb8f30a8030dda9b8fc2653ae
frankos333 <frankos333@gmail.com>
2025-08-18 18:33:22 +0300

    Changes from CR wth yonatan on google meet. these are great changes. we like them very much.
    (#86c4qxme1)

--- norman_objects/authorization/access_token.py
+++ norman_objects/authorization/access_token.py
@@ -24,8 +24,8 @@ class AccessToken(NormanBaseModel):
 
     @property
     def encoded2(self):
-        header_json = json.dumps(self.header, separators=(",", ":")).replace("/", "\\/")
-        payload_json = json.dumps(self.payload, separators=(",", ":")).replace("/", "\\/")
+        header_json = json.dumps(self.header).replace("/", "\\/")
+        payload_json = json.dumps(self.payload).replace("/", "\\/")
 
         header_b64 = base64.urlsafe_b64encode(header_json.encode("utf-8")).rstrip(b"=").decode("utf-8")
         payload_b64 = base64.urlsafe_b64encode(payload_json.encode("utf-8")).rstrip(b"=").decode("utf-8")
