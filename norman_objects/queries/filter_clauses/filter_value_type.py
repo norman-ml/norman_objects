04ac9b92288d434d9c66f518913560db9b588c93
avremy <avremy.back@norman-ai.com>
2025-08-21 18:46:06 +0300

    Simplify imports by removing Python version compatibility handling.
    (#86c50w30u)

--- norman_objects/queries/filter_clauses/filter_value_type.py
+++ norman_objects/queries/filter_clauses/filter_value_type.py
@@ -1,8 +1,5 @@
-try:
-    from typing import TypeAlias, Union
-except ImportError:
-    from typing_extensions import TypeAlias
-    from typing import Union
+from typing_extensions import TypeAlias
+from typing import Union
 
 FilterTypeValue: TypeAlias = Union[str, int, float]
 FilterTypeVar: TypeAlias = Union[FilterTypeValue, list[FilterTypeValue]]
