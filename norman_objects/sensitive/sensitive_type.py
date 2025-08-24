3c6c0dd7b242be1d5579d6611d55961fe51f2a8c
avremy <avremy.back@norman-ai.com>
2025-08-24 23:33:50 +0300

    Add Pydantic core schema support for `SensitiveType` validation.
    (#86c2wv37e)

--- norman_objects/sensitive/sensitive_type.py
+++ norman_objects/sensitive/sensitive_type.py
@@ -1,4 +1,7 @@
 from typing import Type, TypeVar
+
+from pydantic_core import core_schema
+
 from norman_objects.sensitive.sensitive import Sensitive
 
 _sensitive_type_cache = {}
@@ -11,7 +14,12 @@ class SensitiveType:
             return _sensitive_type_cache[inner_type]
 
         class SensitiveCls(Sensitive, inner_type):
-            pass
+            @classmethod
+            def __get_pydantic_core_schema__(cls, _, handler):
+                return core_schema.no_info_after_validator_function(
+                    lambda v: cls(v),
+                    handler.generate_schema(inner_type)
+                )
 
         SensitiveCls.__name__ = f"Sensitive|{inner_type.__name__.capitalize()}"
         _sensitive_type_cache[inner_type] = SensitiveCls
