da7362fc216c9481afdae7238c2b47df77bad9e4
avremy <avremy.back@norman-ai.com>
2025-08-25 19:43:19 +0300

    Add `Literal` entity_type annotations for message classes.
    (#86c2wv37e)

--- norman_objects/messages/norman_message_factory.py
+++ norman_objects/messages/norman_message_factory.py
@@ -5,7 +5,6 @@ from norman_objects.messages.asset_message import AssetMessage
 from norman_objects.messages.input_message import InputMessage
 from norman_objects.messages.invocation_message import InvocationMessage
 from norman_objects.messages.model_message import ModelMessage
-from norman_objects.messages.norman_base_message import NormanBaseMessage
 from norman_objects.messages.output_message import OutputMessage
 
 
