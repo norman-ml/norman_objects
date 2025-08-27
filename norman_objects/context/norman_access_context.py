520529c250a5e482e650d179c390c5cdf7837e42
frankos333 <frankos333@gmail.com>
2025-08-27 21:11:34 +0300

    Changing norman context methods to be simpler and clearer as its clear from the NormanAccessContext what were doing here.
    (#86c4t0jeu)

--- norman_objects/context/norman_access_context.py
+++ norman_objects/context/norman_access_context.py
@@ -8,11 +8,11 @@ class NormanAccessContext:
     _access_token: ContextVar[Optional[JwtToken]] = ContextVar("norman_access_token")
 
     @staticmethod
-    def get_access_token():
+    def get():
         return NormanAccessContext._access_token.get()
 
     @staticmethod
-    def set_access_token(access_token: JwtToken):
+    def set(access_token: JwtToken):
         NormanAccessContext._access_token.set(access_token)
 
     @staticmethod
