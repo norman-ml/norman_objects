341a096825b17f6311b8773de8219305e52464d8
avremy <avremy.back@norman-ai.com>
2025-08-28 10:53:01 +0300

    Refactor subclass initialization using pydantic method override.
    (#86c2wv37e)

--- norman_objects/norman_base_model.py
+++ norman_objects/norman_base_model.py
@@ -5,14 +5,13 @@ from norman_objects.norman_update_schema import NormanUpdateSchema
 
 
 class NormanBaseModel(BaseModel):
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__(**kwargs)
-
+    @classmethod
+    def __pydantic_init_subclass__(cls, **kwargs):
         cls.UpdateSchema: Type[BaseModel] = create_model(
             f"{cls.__name__}Update",
             **{
                 name: (Optional[field.annotation], None)
                 for name, field in cls.model_fields.items()
             },
-            __base__=NormanUpdateSchema
+            __base__=NormanUpdateSchema,
         )
