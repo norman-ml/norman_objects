9f4f51acfc2ec409b0248e57ad9daf2e918755f8
avremy <avremy.back@norman-ai.com>
2025-08-31 23:20:48 +0300

    Pr changes for pydantic 2.11.7 merge into main Moved validator up to follow fields.
    (#86c2wv37e)

--- norman_objects/messages/norman_message_union.py
+++ norman_objects/messages/norman_message_union.py
@@ -1,5 +1,6 @@
 from typing import Union, Annotated
-from pydantic import Field, TypeAdapter
+
+from pydantic import Field
 
 from norman_objects.messages.asset_message import AssetMessage
 from norman_objects.messages.input_message import InputMessage
@@ -7,8 +8,7 @@ from norman_objects.messages.invocation_message import InvocationMessage
 from norman_objects.messages.model_message import ModelMessage
 from norman_objects.messages.output_message import OutputMessage
 
-
-NormanMessage = Annotated[
+NormanMessageUnion = Annotated[
     Union[
         ModelMessage,
         AssetMessage,
@@ -18,14 +18,3 @@ NormanMessage = Annotated[
     ],
     Field(discriminator="entity_type"),
 ]
-
-
-class NormanMessageFactory:
-    @staticmethod
-    def parse_obj(raw_message: dict) -> NormanMessage:
-        if raw_message is None:
-            raise ValueError("Cannot convert None to Norman message")
-        if not isinstance(raw_message, dict):
-            raise ValueError("Cannot convert non-dict to Norman message")
-
-        return TypeAdapter(NormanMessage).validate_python(raw_message)
