6665d7050dc8c1e0108b3b7027f3aafa1c87285a
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-09-04 18:05:53 +0300

    Added default update time factory to status flags.
    Status flag values are now shifted (value + 1) mod 4 to make the error value the largest.
    
    (#86c46r7mp)

--- norman_objects/shared/status_flags/status_flag.py
+++ norman_objects/shared/status_flags/status_flag.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+
+from pydantic import Field
 
 from norman_objects.norman_base_model import NormanBaseModel
 from norman_objects.shared.status_flags.status_flag_value import StatusFlagValue
@@ -7,7 +9,7 @@ from norman_objects.shared.status_flags.status_flag_value import StatusFlagValue
 class StatusFlag(NormanBaseModel):
     id: str = "0"
     account_id: str
-    entity_id: str = "0"
-    update_time: datetime
+    entity_id: str
+    update_time: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     flag_name: str
     flag_value: StatusFlagValue
