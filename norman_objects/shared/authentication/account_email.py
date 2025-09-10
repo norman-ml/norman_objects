0dec9899cf0411626c72ec1867389abf0a1fded5
Dolev <dolev.gabay@norman-ai.com>
2025-09-10 15:46:49 +0300

    Added pointer to account otp in account email instead of credentials hash id.
    (#86c5d72cw)

--- norman_objects/shared/authentication/account_email.py
+++ norman_objects/shared/authentication/account_email.py
@@ -7,7 +7,7 @@ from pydantic import Field
 class AccountEmail(NormanBaseModel):
     id: str = "0"
     account_id: str
-    credential_hash_id: str = "0"
+    account_otp_id: str = "0"
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone(timedelta(0))))
     email: str
     verified: bool = False
