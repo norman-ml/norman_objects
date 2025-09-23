711144954bfda6a71083bd64b4d66b15053a8e86
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-09-24 02:01:51 +0300

    Back status flag name with an enum.
    Fixed some references to old flag names.
    
    (#86c46r7mp)

--- norman_objects/services/file_pull/requests/output_download_request.py
+++ norman_objects/services/file_pull/requests/output_download_request.py
@@ -1,8 +1,11 @@
+from datetime import datetime, timezone
 from typing_extensions import override
 
 from norman_objects.services.file_pull.requests.file_download_request import NormanFileDownloadRequest
 from norman_objects.shared.messages.entity_type import EntityType
 from norman_objects.shared.messages.output_message import OutputMessage
+from norman_objects.shared.status_flags.status_flag import StatusFlag
+from norman_objects.shared.status_flags.status_flag_name import StatusFlagName
 from norman_objects.shared.status_flags.status_flag_value import StatusFlagValue
 
 
@@ -23,3 +26,15 @@ class OutputDownloadRequest(NormanFileDownloadRequest):
     def to_base_message(self, flag_value: StatusFlagValue):
         status_flag = super().to_status_flag(flag_value)
         return OutputMessage.base_message(status_flag)
+
+    @override
+    def to_status_flag(self, flag_value: StatusFlagValue):
+        update_time = datetime.now(timezone.utc)
+
+        return StatusFlag(
+            account_id=self.account_id,
+            entity_id=self.entity_id,
+            update_time=update_time,
+            flag_name=StatusFlagName.Output_EFS_Staging,
+            flag_value=flag_value
+        )
