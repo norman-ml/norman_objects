93b024d29c57b11e06c9b6a9f7c181ae46d63640
avremy <avremy.back@norman-ai.com>
2025-10-06 14:40:24 +0300

    Replaced `DataDomain` with `Modality` and added `model_class` in model.
    (#86c3dfp70)

--- norman_objects/shared/model_signatures/model_signature.py
+++ norman_objects/shared/model_signatures/model_signature.py
@@ -5,7 +5,7 @@ from norman_objects.shared.model_signatures.http_location import HttpLocation
 from norman_objects.shared.model_signatures.receive_format import ReceiveFormat
 from norman_objects.shared.model_signatures.signature_transform import SignatureTransform
 from norman_objects.shared.model_signatures.signature_type import SignatureType
-from norman_objects.shared.parameters.data_domain import DataDomain
+from norman_objects.shared.parameters.modality import Modality
 from norman_objects.shared.parameters.model_param import ModelParam
 
 
@@ -13,7 +13,8 @@ class ModelSignature(NormanBaseModel):
     id: str = "0"
     model_id: str = "0"
     signature_type: SignatureType
-    data_domain: DataDomain
+    modality: Modality
+    data_domain: str
     data_encoding: str
     receive_format: ReceiveFormat
     http_location: HttpLocation
