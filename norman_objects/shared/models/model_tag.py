3d9ddbfdc26ac8f494b31fc69512eeebac499918
avremy <avremy.back@norman-ai.com>
2025-10-21 16:56:50 +0300

    Removed unused `id` and `account_id` fields from `ModelTag` class.
    (#2t83hu9)

--- norman_objects/shared/models/model_tag.py
+++ norman_objects/shared/models/model_tag.py
@@ -2,8 +2,6 @@ from norman_objects.norman_base_model import NormanBaseModel
 
 
 class ModelTag(NormanBaseModel):
-    id: str = "0"
-    account_id: str = "0"
     model_id: str = "0"
-    tag: str
-    tag_count: int
+    tag_name: str
+    tag_count: int = 1
