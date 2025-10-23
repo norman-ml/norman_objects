9a556ebb0db4e9da0d59af0f68d5b3639cf3576e
avremy <avremy.back@norman-ai.com>
2025-10-23 20:38:10 +0300

    Renamed fields in `ModelTag` and updated `ModelPreview` tags structure.
    (#2t83hu9)

--- norman_objects/shared/models/model_tag.py
+++ norman_objects/shared/models/model_tag.py
@@ -3,5 +3,5 @@ from norman_objects.norman_base_model import NormanBaseModel
 
 class ModelTag(NormanBaseModel):
     model_id: str = "0"
-    tag_name: str
-    tag_count: int = 1
+    name: str
+    count: int = 1
