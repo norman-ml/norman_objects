1c952b0a83fadf8bd2abe92085b1fb4578605554
Dolev <dolev.gabay@norman-ai.com>
2025-10-26 22:03:07 +0200

    Adde SignupKeyRequest to wrap the authenticate response in object.
    (#86c4p1j6d)

--- norman_objects/services/authenticate/signup/signup_key_response.py
+++ norman_objects/services/authenticate/signup/signup_key_response.py
@@ -2,7 +2,6 @@ from norman_objects.norman_base_model import NormanBaseModel
 from norman_objects.shared.accounts.account import Account
 
 
-class SignupKeyRequest(NormanBaseModel):
+class SignupKeyResponse(NormanBaseModel):
     account: Account
     api_key: str
-    message: str
