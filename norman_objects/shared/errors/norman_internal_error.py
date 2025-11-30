ed77376a8936c9b5542d647300b02babbbf3ca0c
Aiman <aimanshomre@gmail.com>
2025-11-30 18:54:54 +0200

    Moved around various config blocks to precisely trace the underlying fault. Moved around various config blocks to precisely trace the underlying fault.
    (#86c5dkb5w)

--- norman_objects/shared/errors/norman_internal_error.py
+++ norman_objects/shared/errors/norman_internal_error.py
@@ -1,7 +1,12 @@
 from typing import Optional
 
+from pydantic import ConfigDict
+
 from norman_objects.shared.errors.norman_error import NormanError
+from norman_objects.shared.errors.ttttttt import NormanIntError
 
 
-class NormanInternalError(NormanError):
+class NormanInternalError(NormanIntError):
+    # model_config = ConfigDict(arbitrary_types_allowed=True)
+
     original_exception: Optional[Exception] = None
