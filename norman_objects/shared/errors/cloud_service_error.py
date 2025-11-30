bc458bec8410687726693dd326e4ff921b61fd2c
Aiman <aimanshomre@gmail.com>
2025-11-30 18:34:59 +0200

    Trying moving the config arrount in order to figure out where the error is. Trying moving the config arrount in order to figure out where the error is.
    (#86c5dkb5w)

--- norman_objects/shared/errors/cloud_service_error.py
+++ norman_objects/shared/errors/cloud_service_error.py
@@ -1,5 +1,8 @@
 from norman_objects.shared.errors.infrastructure_error import InfrastructureError
+from pydantic import ConfigDict
 
 
 class CloudServiceError(InfrastructureError):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     pass
