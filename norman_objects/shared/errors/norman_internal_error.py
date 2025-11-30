facbc5c667317d997237e61b6a5e3a197301e4cc
Aiman <aimanshomre@gmail.com>
2025-11-30 18:30:24 +0200

    The config needs to be set on the base NormanError class so it propagates   to all child classes. The config needs to be set on the base NormanError class so it propagates   to all child classes.
    (#86c5dkb5w)

--- norman_objects/shared/errors/norman_internal_error.py
+++ norman_objects/shared/errors/norman_internal_error.py
@@ -1,11 +1,7 @@
 from typing import Optional
 
-from pydantic import ConfigDict
-
 from norman_objects.shared.errors.norman_error import NormanError
 
 
 class NormanInternalError(NormanError):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
     original_exception: Optional[Exception] = None
