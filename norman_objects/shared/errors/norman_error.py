81a8fbdea71c56968f8152b2992eae481e8bba34
Aiman <aimanshomre@gmail.com>
2025-11-30 21:17:21 +0200

    Make NormanError an Exception so internal errors can be raised cleanly. Make NormanError an Exception so internal errors can be raised cleanly.
    (#86c5dkb5w)

--- norman_objects/shared/errors/norman_error.py
+++ norman_objects/shared/errors/norman_error.py
@@ -2,13 +2,14 @@ from datetime import datetime, UTC
 from typing import Optional
 
 
-class NormanError:
+class NormanError(Exception):
     def __init__(
         self,
         message: str,
         details: Optional[dict] = None,
         timestamp: Optional[datetime] = None
     ):
+        super().__init__(message)
         self.message = message
         self.details = details if details is not None else {}
         self.timestamp = timestamp if timestamp is not None else datetime.now(UTC)
