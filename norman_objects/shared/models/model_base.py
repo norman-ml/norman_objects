35e15e4f93f8bcfeb6bc9dbaba8d941925aa1293
ori-small <ori.small@norman-ai.com>
2025-12-01 19:29:29 +0200

    CR fixes: renamed all modelTag and model_tag to modelBaseTag and model_base_tag. CR fixes: renamed all modelTag and model_tag to modelBaseTag and model_base_tag.
    (#2t83hu900)

--- norman_objects/shared/models/model_base.py
+++ norman_objects/shared/models/model_base.py
@@ -1,7 +1,7 @@
 from norman_objects.norman_base_model import NormanBaseModel
 from norman_objects.shared.models.model_preview import ModelPreview
 from norman_objects.shared.tags.aggregate_tag import AggregateTag
-from norman_objects.shared.tags.model_tag import ModelTag
+from norman_objects.shared.tags.model_base_tag import ModelBaseTag
 
 
 class ModelBase(NormanBaseModel):
@@ -11,4 +11,4 @@ class ModelBase(NormanBaseModel):
     invocation_count: int
     model_previews: list[ModelPreview] = []
     tags: list[AggregateTag] = []
-    user_added_tags: list[ModelTag] = []
+    user_added_tags: list[ModelBaseTag] = []
--- norman_objects/shared/tags/model_base_tag.py
+++ norman_objects/shared/tags/model_base_tag.py
@@ -1,7 +1,7 @@
 from norman_objects.norman_base_model import NormanBaseModel
 
 
-class ModelTag(NormanBaseModel):
+class ModelBaseTag(NormanBaseModel):
     id: str = "0"
     account_id: str = "0"
     model_base_id: str = "0"
