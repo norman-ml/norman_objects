5268452c0fbd151b595fe9ede0077f593f081897
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-12-03 03:57:35 +0200

    Initial entity skeleton for revised model stack.
    (#86c6wa00a)

--- norman_objects/shared/models/model_preview.py
+++ norman_objects/shared/models/model_preview.py
@@ -1,24 +1,15 @@
-from datetime import datetime, timezone
-from typing import Optional
-
-from pydantic import Field
-
 from norman_objects.norman_base_model import NormanBaseModel
-from norman_objects.shared.date.normalized_datetime import NormalizedDateTime
 from norman_objects.shared.models.aggregate_tag import AggregateTag
-from norman_objects.shared.models.model_asset import ModelAsset
-from norman_objects.shared.models.model_build_status import ModelBuildStatus
+from norman_objects.shared.models.model_version_preview import ModelVersionPreview
 
 
 class ModelPreview(NormanBaseModel):
-    id: str = "0"
+    id: str
     account_id: str
-    model_base_id: str = "0"
-    version_label: str
-    active: bool = True
-    build_status: Optional[ModelBuildStatus] = None
-    creation_time: NormalizedDateTime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    invocation_count: int
+
+    name: str
     short_description: str
 
-    assets: list[ModelAsset] = []
-    tags: list[AggregateTag] = []
+    versions: list[ModelVersionPreview] = []
+    aggregate_tags: list[AggregateTag] = []
