c15754ad97ba84f4f0410ebe622fde0cb67caf1f
Aiman <aimanshomre@gmail.com>
2025-12-04 17:22:39 +0200

    Intermediate commit following updates to ExceptionFactory and NormanException. Intermediate commit following updates to ExceptionFactory and NormanException.
    (#86c5dkb5w)

--- norman_objects/shared/exceptions/norman_exception.py
+++ norman_objects/shared/exceptions/norman_exception.py
@@ -1,16 +1,22 @@
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 from typing import Optional
+from norman_objects.shared.date.normalized_datetime import NormalizedDateTime
 
 
 class NormanException(Exception):
-
     def __init__(
         self,
         message: str,
         context: Optional[dict] = None,
-        timestamp: Optional[datetime] = None
+        timestamp: Optional[NormalizedDateTime] = None
     ):
         Exception.__init__(self, message)
         self.message = message
-        self.context = context if context is not None else {}
-        self.timestamp = timestamp if timestamp is not None else datetime.now(UTC)
+
+        if context is None:
+            context = {}
+        self.context = context
+
+        if timestamp is None:
+            timestamp = datetime.now(timezone.utc)
+        self.timestamp = timestamp
