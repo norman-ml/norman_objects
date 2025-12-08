2985cc459386783362647e88753a2303d7c89018
ori-small <ori.small@norman-ai.com>
2025-12-08 23:15:16 +0200

    CR fixes: added a small naming fix for Authorization audiences enum. CR fixes: added a small naming fix for Authorization audiences enum.
    (#86c5f96kw)

--- norman_objects/shared/security/authorization_audiences.py
+++ norman_objects/shared/security/authorization_audiences.py
@@ -1,6 +1,6 @@
 from enum import StrEnum
 
-class Audiences(StrEnum):
+class AuthorizationAudiences(StrEnum):
     client = "norman:client"
     quotas = "norman:quotas"
     server = "norman:server"
