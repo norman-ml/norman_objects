28f6d30f8bd2d3af457d958badd356a6adb4787d
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-16 04:10:49 +0200

    Adding entity ID instead of the entire pairing request. Adding entity ID instead of the entire pairing request.
    (#86c5cuvtx)

--- norman_objects/services/file_push/checksum/checksum_request.py
+++ norman_objects/services/file_push/checksum/checksum_request.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
 
-from norman_objects.services.file_push.pairing.socket_pairing_request import SocketPairingRequest
-
 
 class ChecksumRequest(BaseModel):
-    pairing_request: SocketPairingRequest
     pairing_id: str
     checksum: str
+    entity_id: str # Used for trace id derivation
