d6f9b3b34d12e0f2c2c3a1200d36c52d2d10a127
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-16 04:15:22 +0200

    Making entity id first, because he is the id of the thing, so he should be first of course.. Making entity id first, because he is the id of the thing, so he should be first of course..
    (#86c5cuvtx)

--- norman_objects/services/file_push/checksum/checksum_request.py
+++ norman_objects/services/file_push/checksum/checksum_request.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class ChecksumRequest(BaseModel):
+    entity_id: str # Used for trace id derivation
     pairing_id: str
     checksum: str
-    entity_id: str # Used for trace id derivation
