194176a90ee2f83e9517f118a2c226988839e03b
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-12-16 23:01:27 +0200

    Added model projection class as a first-order entity spanning a model and a single version.
    (#86c0d7f90)

--- norman_objects/shared/models/model_projection.py
+++ norman_objects/shared/models/model_projection.py
@@ -2,14 +2,14 @@ from datetime import datetime, timezone
 
 from pydantic import Field
 
+from norman_objects.norman_base_model import NormanBaseModel
 from norman_objects.shared.date.normalized_datetime import NormalizedDateTime
 from norman_objects.shared.models.aggregate_tag import AggregateTag
-from norman_objects.shared.models.model_preview import ModelPreview
 from norman_objects.shared.models.model_tag import ModelTag
 from norman_objects.shared.models.model_version import ModelVersion
 
 
-class Model(ModelPreview):
+class ModelProjection(NormanBaseModel):
     id: str = "0"
     account_id: str
     creation_time: NormalizedDateTime = Field(default_factory=lambda: datetime.now(timezone.utc))
@@ -18,6 +18,6 @@ class Model(ModelPreview):
     category: str
     invocation_count: int
 
-    versions: list[ModelVersion] = []
+    version: ModelVersion
     aggregate_tags: list[AggregateTag] = []
     user_tags: list[ModelTag] = []
