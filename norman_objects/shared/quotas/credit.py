6a8d92bf58daf43addfdf79319a0aece060bc23e
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-24 12:23:34 +0200

    Added invocation ids to credit.py in order to connect between the two entities. Added invocation ids to credit.py in order to connect between the two entities.
    (#86c6t5j6h)

--- norman_objects/shared/quotas/credit.py
+++ norman_objects/shared/quotas/credit.py
@@ -8,15 +8,17 @@ from norman_objects.shared.date.normalized_datetime import NormalizedDateTime
 class Credit(BaseModel):
     id: str = "0"
     quota_id: str
+    invocation_id: str
     start_date: NormalizedDateTime
     end_date: NormalizedDateTime
     billable: bool
 
     @classmethod
-    def now(cls, quota_id: str, duration_seconds: int, billable: bool):
+    def now(cls, quota_id: str, invocation_id: str, duration_seconds: int, billable: bool):
         now = datetime.now(timezone.utc)
         return cls(
             quota_id=quota_id,
+            invocation_id=invocation_id,
             start_date=now,
             end_date=now + timedelta(seconds=duration_seconds),
             billable=billable
