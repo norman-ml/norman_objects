771b3ce159cb6364d6bc9fd23c700c2885425ddc
ori-small <ori.small@norman-ai.com>
2025-12-29 20:44:41 +0200

    CR fixes: added os_version to model version entity. CR fixes: added os_version to model version entity.
    (#86c246m16)

--- norman_objects/shared/models/model_version.py
+++ norman_objects/shared/models/model_version.py
@@ -29,6 +29,7 @@ class ModelVersion(ModelVersionPreview):
 
     cuda_version: str
     python_version: str
+    os_version: str
 
     hosting_location: ModelHostingLocation
     model_type: ModelType
