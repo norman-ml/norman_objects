5ba5969d8f024d9815a032fdcec3159054b02000
ori-small <ori.small@norman-ai.com>
2026-01-01 14:26:26 +0200

    CR fixes: fixed download requests and tracked downloads to use the Asset_EFS_Staging flag. CR fixes: fixed download requests and tracked downloads to use the Asset_EFS_Staging flag.
    (#86c246m16)

--- norman_objects/services/file_pull/download/tracked_asset_download.py
+++ norman_objects/services/file_pull/download/tracked_asset_download.py
@@ -31,7 +31,7 @@ class TrackedAssetDownload(TrackedDownload):
         access_token = NormanAccessContext.get()
         update_time = datetime.now(timezone.utc)
 
-        flag_name = StatusFlagName[f"{self.asset.asset_name}_EFS_Staging"]
+        flag_name = StatusFlagName["Asset_EFS_Staging"]
 
         sns_message = AssetMessage(
             access_token=access_token,
