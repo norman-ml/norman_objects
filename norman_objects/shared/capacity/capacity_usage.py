0de54981f9fff17947fcdff081cef459314d18eb
ori-small <ori.small@norman-ai.com>
2026-02-16 17:22:41 +0200

    Made small adjustments to the order of variables in capacity_usage.py. Made small adjustments to the order of variables in capacity_usage.py.
    (#86c72xpb1)

--- norman_objects/shared/capacity/capacity_usage.py
+++ norman_objects/shared/capacity/capacity_usage.py
@@ -2,9 +2,9 @@ from norman_objects.norman_base_model import NormanBaseModel
 
 
 class CapacityUsage(NormanBaseModel):
-    version_id: str
-    model_id: str
     account_id: str
+    model_id: str
+    version_id: str
     machine_type: str
     capacity: int
     active: bool
