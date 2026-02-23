718b347a6a96e151e93e70ae0dcbaac2b016fc56
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-02-23 12:49:46 +0200

    Reintroduced integer and float modalities for containers and channels.
    (#86c6x1m7k)

--- norman_objects/shared/modality/channel_modality.py
+++ norman_objects/shared/modality/channel_modality.py
@@ -3,6 +3,8 @@ from enum import Enum
 
 class ChannelModality(str, Enum):
     Audio = "Audio"
+    Float = "Float"
     Image = "Image"
+    Integer = "Integer"
     Text = "Text"
     Video = "Video"
--- norman_objects/shared/modality/container_modality.py
+++ norman_objects/shared/modality/container_modality.py
@@ -5,14 +5,16 @@ from functools import cache
 class ContainerModality(str, Enum):
     Audio = "Audio"
     File = "File"
+    Float = "Float"
     Image = "Image"
+    Integer = "Integer"
     Text = "Text"
     Video = "Video"
 
     @staticmethod
     @cache
     def primitive_types():
-        return {ContainerModality.Text}
+        return {ContainerModality.Float, ContainerModality.Integer, ContainerModality.Text}
 
     def is_primitive(self):
         return self in ContainerModality.primitive_types()
