e8119a53c0e02346a683fdaed2ca06159a172e05
ori-small <ori.small@norman-ai.com>
2026-02-26 18:58:08 +0200

    CR fixes: removed account id default value for tags. CR fixes: removed account id default value for tags.
    (#86c72xpb1)

--- norman_objects/shared/models/model_tag.py
+++ norman_objects/shared/models/model_tag.py
@@ -3,6 +3,6 @@ from norman_objects.norman_base_model import NormanBaseModel
 
 class ModelTag(NormanBaseModel):
     id: str = "0"
-    account_id: str = "0"
+    account_id: str
     model_id: str = "0"
     name: str
