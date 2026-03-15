6795462f0937d1f3dbf19e1df207ebecb9653503
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-03-15 22:40:20 +0200

    Move provisioning from objects internal to objects library.
    (#86c778p0x)

---
+++ norman_objects/shared/provisioning/provisioned_instance.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from norman_objects.norman_base_model import NormanBaseModel
+
+from norman_objects_internal.shared.provisioning.provisioned_instance_status import ProvisionedInstanceStatus
+
+
+class ProvisionedInstance(NormanBaseModel):
+    id: str = "0"
+    account_id: str
+    model_id: str
+    version_id: str
+    ec2_instance_id: Optional[str] = None
+    status: ProvisionedInstanceStatus
