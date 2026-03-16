aaeb45c22e8ff9222f1325a790409c903237303a
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-03-16 19:14:12 +0200

    Added file representation class to norman objects.
    (#86c778p0x)

---
+++ norman_objects/shared/representation/file_representation.py
@@ -0,0 +1,17 @@
+from norman_objects.norman_base_model import NormanBaseModel
+from norman_objects.shared.encoding.container_encoding import ContainerEncoding
+from norman_objects.shared.modality.container_modality import ContainerModality
+
+
+class FileRepresentation(NormanBaseModel):
+    container_modality: ContainerModality
+    container_encoding: ContainerEncoding
+    mime_type: str
+
+    def to_dict(self):
+        return {
+            "container_modality": self.container_modality,
+            "container_encoding": self.container_encoding,
+            "mime-type": self.mime_type,  # HTTP relies on a mime-type header for content type resolving.
+            "Content-Type": self.mime_type  # S3 relies on Content-Type for proper file handling.
+        }
