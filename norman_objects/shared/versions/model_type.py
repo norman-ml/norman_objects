671c9ad11cbb9c89c10a40bfbbc7b0988d35f84d
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-04-07 02:18:19 +0300

    ...and add the missing Pytorch_Script literal.
    (#86c72xnkv)

--- norman_objects/shared/versions/model_type.py
+++ norman_objects/shared/versions/model_type.py
@@ -4,7 +4,7 @@ from enum import Enum
 class ModelType(str, Enum):
     Api = "Api"
     Pytorch_Export = "Pytorch_Export"
-    Pytorch_jit = "Pytorch_jit"
+    Pytorch_Script = "Pytorch_Script"
     HuggingFace_Diffuser = "HuggingFace_Diffuser"
     HuggingFace_Transformer = "HuggingFace_Transformer"
     HuggingFace_Generic = "HuggingFace_Generic"
