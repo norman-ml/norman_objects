519a5c0b469cfbd310aaf700c4327af52470e81d
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-04-07 02:17:19 +0300

    Expand model type enum with the added pytorch and huggingface model variations.
    (#86c72xnkv)

--- norman_objects/shared/versions/model_type.py
+++ norman_objects/shared/versions/model_type.py
@@ -3,5 +3,9 @@ from enum import Enum
 
 class ModelType(str, Enum):
     Api = "Api"
+    Pytorch_Export = "Pytorch_Export"
     Pytorch_jit = "Pytorch_jit"
-    HuggingFace = "HuggingFace"
+    HuggingFace_Diffuser = "HuggingFace_Diffuser"
+    HuggingFace_Transformer = "HuggingFace_Transformer"
+    HuggingFace_Generic = "HuggingFace_Generic"
+    Other = "Other"
